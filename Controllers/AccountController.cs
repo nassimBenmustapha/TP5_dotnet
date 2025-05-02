5dbd5cade9144614c9060e7c2f6459a5b1a81ceb
nassim <nassimbenmustpha99@gmail.com>
2025-05-02 18:45:46 +0100

    init

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TP5.Data;
+
+namespace TP5.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        // Afficher la liste des utilisateurs
+        public IActionResult UserManagement()
+        {
+            //var users = _userManager.Users.ToList();
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+    }
+
+}
